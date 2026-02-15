fadc0e788bd3994633e97998fa2f1d4615aed82f
Taha Nawfal <34232047+tahanawfal@users.noreply.github.com>
2026-02-15 22:25:52 +0000

    test the model

--- 04-analytics-engineering/models/staging/green_tripdata.sql
+++ 04-analytics-engineering/models/staging/green_tripdata.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='view') }}
 
-SELECT * FROM {{ source('raw_data', 'external_green_taxi') }}
+SELECT * FROM {{ source('raw_data', 'green_taxi_trips') }}
 WHERE lpep_pickup_datetime >= '2019-01-01'
   AND lpep_pickup_datetime < '2019-02-01'
